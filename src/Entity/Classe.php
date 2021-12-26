283c92b2c84e1c4c30bfa6a7daa65759893e6598
DSI4k9 <fahd.devb@gmail.com>
2021-12-26 19:29:50 +0000

    Entity and controller classe

---
+++ src/Entity/Classe.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClasseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClasseRepository::class)
+ */
+class Classe
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $niveau;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $anneeuniver;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(string $niveau): self
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+
+    public function getAnneeuniver(): ?\DateTimeInterface
+    {
+        return $this->anneeuniver;
+    }
+
+    public function setAnneeuniver(\DateTimeInterface $anneeuniver): self
+    {
+        $this->anneeuniver = $anneeuniver;
+
+        return $this;
+    }
+}
